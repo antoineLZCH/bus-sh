afe79fbe0819b31d7e5a0e683a194f88e122bb41
Antoine LOZACH <antoinelozach.pro@gmail.com>
2020-11-07 22:10:12 +0100

    WORKING ?

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
+const path = require('path');
 const express = require('express');
 const helmet = require('helmet');
 const morgan = require('morgan');
 const cors = require('cors');
 const yup = require('yup');
+const {nanoid} = require('nanoid');
+const monk = require('monk');
+const rateLimit = require('express-rate-limit');
+const slowDown = require('express-slow-down');
+
+if (process.env.NODE_END !== 'production') {
+    require('dotenv').config();
+}
+
+const db = monk(process.env.MONGO_URI);
+const urls = db.get('urls');
+urls.createIndex({slug: 1}, {unique: true});
 
 const app = express();
+app.enable('trust proxy');
 
 app.use(helmet());
 app.use(morgan('tiny'));
@@ -12,19 +26,82 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static('./public'));
 
-app.get('/url/:id', (req, res) => {
-  // TODO : Get Short URL by Id
+const schema = yup.object().shape({
+    slug: yup.string().trim().matches(/^[\w\-]+$/i),
+    url: yup.string().trim().url().required(),
 });
 
-app.get('/:id', (req, res) => {
-  // TODO : Redirect to URL
+const notFoundPath = path.join(__dirname, 'public/404.html');
+
+app.get('/:id', async (req, res, next) => {
+    const {id: slug} = req.params;
+    try {
+        const url = await urls.findOne({slug});
+        if (url) {
+            return res.redirect(url.url);
+        }
+        return res.status(404).sendFile(notFoundPath);
+    } catch (error) {
+        return res.status(404).sendFile(notFoundPath);
+    }
 });
 
-app.post('/:id', (req, res) => {
-  // TODO : Create short URL
+app.post('/url', slowDown({
+    windowMs: 30 * 1000,
+    delayAfter: 1,
+    delayMs: 500,
+}), rateLimit({
+    windowMs: 30 * 1000,
+    max: 1,
+}), async (req, res, next) => {
+    let {slug, url} = req.body;
+    try {
+        await schema.validate({
+            slug,
+            url,
+        });
+        if (url.includes('cdg.sh')) {
+            throw new Error('Stop it. 🛑');
+        }
+        if (!slug) {
+            slug = nanoid(5);
+        } else {
+            const existing = await urls.findOne({slug});
+            if (existing) {
+                throw new Error('Slug in use. 🍔');
+            }
+        }
+        slug = slug.toLowerCase();
+        const newUrl = {
+            url,
+            slug,
+        };
+        const created = await urls.insert(newUrl);
+        res.json(created);
+    } catch (error) {
+        next(error);
+    }
 });
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(notFoundPath);
+})
+
+
+app.use((error, req, res, next) => {
+    if (error.status) {
+        res.status(error.status);
+    } else {
+        res.status(500);
+    }
+    res.json({
+        message: error.message,
+        stack: process.env.NODE_ENV === 'production' ? '🥞' : error.stack,
+    });
+});
+
 
 const port = process.env.PORT || 1337;
 app.listen(port, () => {
-  console.log(`Listening on ${port}`);
+    console.log(`Listening on ${port}`);
 })
